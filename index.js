42b6e8584920a6470302c08ffdd96db86009d8d8
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2022-06-29 15:27:16 +0530

    update changes

--- index.js
+++ index.js
@@ -1,88 +1,102 @@
 function getWordDefinitions(cell) {
     const meanings = getResponseFromApi(cell)
-    const objects = meanings.map((meaning) => {
-        const definitions = meaning.definitions
-        definitionsString = definitions.map(def => def?.definition).join('; ')
-        return definitionsString
+    const objects = meanings.map((meaning)=>{
+      const definitions = meaning.definitions
+      definitionsString = definitions.map(def=>def?.definition).join('; ')
+      return definitionsString
     })
     return objects.join('; ')
-}
-function getWordSynonyms(cell) {
+  }
+  function getWordSynonyms(cell) {
     const meanings = getResponseFromApi(cell)
-    const objects = meanings.map((meaning) => {
-        const definitions = meaning.definitions
-        synonyms = definitions.map(def => def?.synonyms)
-        meaning.synonyms.forEach(syn => {
-            if (!syn in synonyms) {
-                synonyms.push(syn)
-            }
-        })
-        return synonyms.length ? synonyms.join(' ') : ""
+    const objects = meanings.map((meaning)=>{
+      const definitions = meaning.definitions
+      synonyms = definitions.map(def=>def?.synonyms)
+      meaning.synonyms.forEach(syn => {
+        if(!syn in synonyms){
+          synonyms.push(syn)
+        }
+      })
+      return synonyms.length ? synonyms.join(' ') : ""
+    })
+    return objects
+  }
+  function getWordExamples(cell) {
+    const meanings = getResponseFromApi(cell)
+    const objects = meanings?.map((meaning)=>{
+      const definitions = meaning.definitions
+      examples = definitions.map(def=>def?.example)
+      return examples.length ? examples.join(' ') : ""
     })
     return objects.length ? objects.join(' ') : ""
-}
-function getWordExamples(cell) {
-    const meanings = getResponseFromApi(cell)
-    const objects = meanings?.map((meaning) => {
-        const definitions = meaning.definitions
-        examples = definitions.map(def => def?.example)
-        return examples.length ? examples.join(' ') : ""
-    })
-    return objects.length ? objects.join(' ') : ""
-}
-
-function getResponseFromApi(word) {
+  }
+  
+  function getResponseFromApi (word){
     var url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
     const rsp = UrlFetchApp.fetch(url)
     const json = JSON.parse(rsp.getContentText())
     return json[0].meanings
-}
-
-function getResponseFromApiv2(word) {
+  }
+  
+  function getResponseFromApiv2(word){
     let options = {
-        "async": true,
-        "crossDomain": true,
-        "method": "GET",
-        "headers": {
-            "X-RapidAPI-Key": "X_RAPID_API_KEY",
-            "cache-control": "no-cache"
-        }
-    };
-    try {
+       "async": true,
+       "crossDomain": true,
+       "method" : "GET",
+       "headers" : {
+         "X-RapidAPI-Key" : "f9dace07dbmsh003841c3c16ae3bp12bc85jsn5472b5bffc09",
+         "cache-control": "no-cache"
+       }
+     };
+     try{
         var url = `https://wordsapiv1.p.rapidapi.com/words/${word}`
         const rsp = UrlFetchApp.fetch(url, options)
         const json = JSON.parse(rsp.getContentText())
-        if (json.success != false && json.results !== undefined) {
-            return { results: json.results, api: 'wordsapi' }
-        } else {
-            return { results: getResponseFromApi(word), api: 'dictionaryapi' }
+        if(json.success!=false && json.results!==undefined){
+          return {results : json.results, api:'wordsapi'}
+        }else{
+          return {results : getResponseFromApi(word), api:'dictionaryapi'}
         }
-    } catch (err) {
-        return { results: getResponseFromApi(word), api: 'dictionaryapi' }
-    }
-}
-
-function getWordDefinitionsv2(cell) {
-    const { results, api } = getResponseFromApiv2(cell)
-    if (api === 'dictionaryapi') {
+     }catch(err){
+       return {results : getResponseFromApi(word), api:'dictionaryapi'}
+     }
+  }
+  
+  function getMeaning(cell){
+    try{
+      const {results, api} = getResponseFromApiv2(cell)
+      if(api ==='dictionaryapi'){
         return getWordDefinitions(cell)
+      }
+      const defs = results.map((res)=>res?.definition)
+      return defs.length ? defs.join('; ') : ""
+    }catch(e){
+      return "-"
     }
-    const defs = results.map((res) => res?.definition)
-    return defs.length ? defs.join('; ') : ""
-}
-function getWordSynonymsv2(cell) {
-    const { results, api } = getResponseFromApiv2(cell)
-    if (api === 'dictionaryapi') {
-        return getWordSynonyms(cell)
+  }
+  function getSynonyms(cell){
+    try{
+      const {results} = getResponseFromApiv2(cell)
+      let synonyms = results.flatMap((res)=>res?.synonyms).filter(ex => ex?.length > 0)
+      getWordSynonyms(cell).forEach(syn => {
+        if(!syn in synonyms){
+          synonyms.push(syn)
+        }
+      })
+      return synonyms.length ? synonyms.join('; ') : ""
+    }catch(e){
+      return "-"
     }
-    const synonyms = results.flatMap((res) => res?.synonyms).filter(ex => ex?.length > 0)
-    return synonyms.length ? synonyms.join('; ') : ""
-}
-function getWordExamplesv2(cell) {
-    const { results, api } = getResponseFromApiv2(cell)
-    if (api === 'dictionaryapi') {
+  }
+  function getEgs(cell){
+    try{
+      const {results, api} = getResponseFromApiv2(cell)
+      if(api ==='dictionaryapi'){
         return getWordExamples(cell)
+      }
+      const exs = results.flatMap((res)=>res?.examples).filter(ex => ex?.length > 0)
+      return exs.length ? exs.join('; ') : ""
+    }catch(e){
+      return "-"
     }
-    const exs = results.flatMap((res) => res?.examples).filter(ex => ex?.length > 0)
-    return exs.length ? exs.join('; ') : ""
-}
+  }
